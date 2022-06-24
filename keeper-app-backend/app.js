48e9488247ca8a2355fc21ff3894cad390f324f0
Sai Pranay <43415112+pranayreddy45@users.noreply.github.com>
2022-06-24 12:51:05 +0530

    Initial Commit

---
+++ keeper-app-backend/app.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+const cookieParser = require("cookie-parser");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(cookieParser());
+
+mongoose.connect("mongodb://localhost:27017/keeperApp", {
+  useNewUrlParser: true,
+});
+
+const keeperAppCookieSchema = new mongoose.Schema({
+  cookieValue: String,
+});
+
+const keeperAppCookie = mongoose.model("usercookie", keeperAppCookieSchema);
+
+const keeperAppSchema = new mongoose.Schema({
+  id: String,
+  title: String,
+  content: String,
+  userCookieValue: String,
+});
+
+const KeeperApp = mongoose.model("note", keeperAppSchema);
+
+let cookieUserData = "";
+
+app.post("/cookie-data", (req, res) => {
+  // console.log(req.body);
+  cookieUserData = req.body.cookieUserData;
+  keeperAppCookie.find({ cookieValue: cookieUserData }, (err, docs) => {
+    if (docs.length === 0) {
+      keeperAppCookie.insertMany([{ cookieValue: cookieUserData }], (error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Cookie Value inserted Sucessfully");
+        }
+      });
+    } else {
+      // console.log("else: " + docs);
+    }
+  });
+});
+
+app.get("/home", (req, res) => {
+  KeeperApp.find({ userCookieValue: cookieUserData }, (err, foundItems) => {
+    res.send(foundItems);
+  });
+});
+
+app.post("/post_name", (req, res) => {
+  let { note } = req.body;
+  console.log("Name is: " + note.id + " " + note.title + " " + note.content);
+  KeeperApp.insertMany(
+    [
+      {
+        id: note.id,
+        title: note.title,
+        content: note.content,
+        userCookieValue: cookieUserData,
+      },
+    ],
+    (error) => {
+      if (error) console.log(error);
+      else console.log("Inserted Sucessfully.");
+    }
+  );
+});
+
+app.post("/delete", (req, res) => {
+  let { id } = req.body;
+  KeeperApp.deleteOne({ id: id })
+    .then(() => {
+      console.log("Deleted Sucessfullu");
+    })
+    .catch((error) => {
+      console.log("Delete Error: " + error);
+    });
+});
+
+var port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log("Running on Port" + port);
+});
